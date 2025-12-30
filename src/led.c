415a9a2d69019154685e338ba0ad27e84a2220da
creyes2001 <reyescristopher43@gmail.com>
2025-12-29 19:23:45 -0600

    feat: implement Led control using gpio HAl

--- src/led.c
+++ src/led.c
@@ -1,19 +1,68 @@
 #include "led.h"
 #include "gpio.h"
+#include <xc.h>
+
+gpio_t led1 = {
+	.port = &PORTD,
+	.tris = &TRISD,
+	.lat = &LATD,
+	.pin = 0
+};
+
+gpio_t led2 = {
+	.port = &PORTD,
+	.tris = &TRISD,
+	.lat = &LATD,
+	.pin = 1
+};
 
 void led_int(void){
-	//TODO:implement
+	//config and set as low both led's
+	gpio_init(&led1,GPIO_OUTPUT);
+	gpio_write(&led1,GPIO_LOW);
+	gpio_init(&led2,GPIO_OUTPUT);
+	gpio_write(&led2,GPIO_LOW);
 }
 
 void led_on(uint8_t led_id){
-	//TODO:implement
+	switch(led_id)
+	{
+		case 0:
+			gpio_write(&led1,GPIO_HIGH);
+			break;
+		case 1:
+			gpio_write(&led2,GPIO_HIGH);
+			break;
+		default:
+			break;
+	}
 }
 
 void led_off(uint8_t led_id){
-	//TODO:implement
+	switch(led_id)
+	{
+		case 0:
+			gpio_write(&led1,GPIO_LOW);
+			break;
+		case 1:
+			gpio_write(&led2,GPIO_LOW);
+			break;
+		default:
+			break;
+	}
 }
 
 void led_toggle(uint8_t led_id){
-	//TODO:implement
+	switch(led_id)
+	{
+		case 0:
+			gpio_toggle(&led1);
+			break;
+		case 1:
+			gpio_toggle(&led2);
+			break;
+		default:
+			break;
+	}
 }
 
